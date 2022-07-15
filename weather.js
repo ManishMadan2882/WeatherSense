d420dbd0c0d3ebaa12e1ffa71d9437f0e1747c86
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-07-16 02:55:54 +0530

    Uploading the files

---
+++ weather.js
@@ -0,0 +1,91 @@
+
+const currentDetails="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key}";
+const currTime = document.querySelector('#time');
+const ampm  = document.getElementById('#ampm');
+const searchBar=document.querySelector('#searchbar');
+const humid = document.getElementById('humidity');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const pressure = document.getElementById('pressure');
+const speed = document.getElementById('speed');
+const temperature = document.getElementById('temperature');
+const feelsLike=document.getElementById('feels-like');
+const loc=document.querySelector('#location span');
+const icon=document.getElementById('icon');
+const weather=document.getElementById('weather');
+const dtInfo = document.getElementById('dtInfo');
+
+const stat = document.getElementById('stat');
+var week = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var year = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
+function format(n){
+   if(n<=9)
+   return "0"+n.toString();
+   else return n.toString();
+}
+
+
+var limit=5;
+var APIkey='bf8d15a80c89aa4f4c82ad6cbb3f5ac5';
+navigator.geolocation.getCurrentPosition((position)=>{
+    var lat=position.coords.latitude;
+    var lon=position.coords.longitude;
+ updateDetails(lat,lon);
+});
+
+async function getCoords(cityName){
+  const limit=5;  
+  const pos = {latitude:0, longitude:0};
+  const api_url=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${APIkey}`
+  await fetch(api_url).then(res=>res.json()).then(data=>{
+   // console.log(data[0].lat,data[0].lon);
+   pos.latitude=data[0].lat;
+   pos.longitude=data[0].lon;
+   return pos;
+    });
+return pos;
+}
+
+document.getElementById('button-addon2').addEventListener('click',()=>{
+    var coords=getCoords(input_text.value);
+    getCoords(input_text.value).then((data)=>{
+    lat=data.latitude;
+    lon=data.longitude;
+    updateDetails(lat,lon);
+    });
+});
+function updateDetails(lat,lon){
+   const weatherAPI=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+   const geoAPI=`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${APIkey}`;
+    fetch(geoAPI).then(res=>res.json()).then(data =>{
+    console.log(data[0].name,data[0].country);  
+    loc.innerHTML=`${data[0].name}, ${data[0].country}`;
+ });
+   fetch(weatherAPI).then(res=>res.json()).then(data=>{
+     
+      icon.src=`http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
+      dt.innerText=new Date(new Date(1000*data.current.dt + data.timezone_offset*1000).getTime()).toUTCString().slice(0,22);
+      stat.innerText=data.current.weather[0].main;
+      humid.innerText=data.current.humidity+" %";
+      temperature.innerText=Number(data.current.temp - 273.15).toFixed(0)+" \u00B0"+"c";
+      feelsLike.innerText=Number(data.current.feels_like - 273.15).toFixed(1);
+      pressure.innerText=data.current.pressure + " mbar";
+      speed.innerText=data.current.wind_speed + " km/h";
+      sunset.innerText=new Date(new Date(1000*data.current.sunset + data.timezone_offset*1000).getTime()).toUTCString().slice(16,22);
+      sunrise.innerText=new Date(new Date(1000*data.current.sunrise + data.timezone_offset*1000).getTime()).toUTCString().slice(16,22);
+      for (let index = 0; index < 7; index++) {
+         let head=document.getElementById(`${index}`).getElementsByTagName("h2")[0];
+         let weatherIcon=document.getElementById(`${index}`).getElementsByTagName("img")[0];
+         let max=document.getElementById(`${index}`).getElementsByTagName("td")[0];
+         let min=document.getElementById(`${index}`).getElementsByTagName("td")[1];
+
+         weatherIcon.src=`http://openweathermap.org/img/wn/${data.daily[`${index}`].weather[0].icon}@2x.png`;
+         head.innerHTML=new Date(new Date(1000*data.daily[index].dt + data.timezone_offset*1000).getTime()).toUTCString().slice(0,11);
+         max.innerHTML=(data.daily[index].temp.max -273.15).toFixed(1);
+         min.innerHTML=(data.daily[index].temp.min -273.15).toFixed(1);
+        
+      }
+   });
+   
+}
+//https://github.com/ManishMadan2882
